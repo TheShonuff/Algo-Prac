d79d7a604464d7c1ab99b5c200368530fa050230
Joe Irvine <jirvine124@gmail.com>
2022-07-14 15:57:32 -0700

    alphabet position

---
+++ CodeWars/alphabetPosition.js
@@ -0,0 +1,19 @@
+function alphabetPosition(text) {
+  const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+  const alphabet = alpha.map((x) => String.fromCharCode(x));
+  const result = [];
+
+  for (let i = 0; i < text.length; i++) {
+    let position = (element) =>
+      element.toLowerCase() === text.charAt(i).toLowerCase();
+    if (alphabet.findIndex(position) >= 0) {
+      result.push(alphabet.findIndex(position) + 1);
+    } else if (alphabet.findIndex(position) === -1) {
+      continue;
+    }
+  }
+  return result.join(" ");
+}
+
+const string = "The narwhal bacons at midnight.";
+console.log(alphabetPosition(string));
